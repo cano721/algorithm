daeebcffa6632aabf4231cab474e4080f7da3c44
cano721 <cano721@naver.com>
2021-08-27 23:37:32 +0900

    IOIOI

---
+++ baekjoon/bj5525_IOIOI.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+m = int(sys.stdin.readline().rstrip())
+s = sys.stdin.readline().rstrip()
+
+nstring = "I" +"OI"*n
+answer = 0
+print(s.count(nstring))
+# for i in range(len(s)):
+#     if i + len(nstring) <= len(s):
+#         if s[i:i + len(nstring)] == nstring:
+#             answer +=1
+# print(answer)
