cc3a4dbb51e72d9cfac1b2f604304fc77966ecca
cano721 <cano721@naver.com>
2021-10-08 22:24:12 +0900

    Java 문자열 예제

---
+++ Java/BJ11654_아스키코드.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+
+public class BJ11654_아스키코드 {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int askii = sc.next().charAt(0);
+        
+        System.out.println(askii);
+    }
+}
