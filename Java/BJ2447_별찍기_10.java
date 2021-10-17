0a3c899c569eb7f564bf246c1f0a7ee219547e14
cano721 <cano721@naver.com>
2021-10-17 22:09:30 +0900

    java 재귀 예제;

--- Java/BJ2447_별찍기_10.java
+++ Java/BJ2447_별찍기_10.java
@@ -1,19 +1,67 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class BJ2447_별찍기_10 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static char[][] arr;
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+ 
+		arr = new char[N][N];
+        
+		star(0, 0, N, false);
+ 
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(arr[i][j]);
+			}
+			sb.append('\n');
+		}
+		System.out.print(sb);
+	}
+ 
+	static void star(int x, int y, int N, boolean blank) {
+ 
+		// 공백칸일 경우
+		if (blank) {
+			for (int i = x; i < x + N; i++) {
+				for (int j = y; j < y + N; j++) {
+					arr[i][j] = ' ';
+				}
 
-        int n = Integer.parseInt(br.readLine());
-        System.out.println(fibo(n));
-    }
-
-    public static int fibo(int num){
-        if(num == 0) return 0;
-
-        if(num == 1) return 1;
-
-        return fibo(num-1)+fibo(num-2);
-    }
+                
+			}
+			return;
+		}
+ 
+		// 더이상 쪼갤 수 없는 블록일 때
+		if (N == 1) {
+			arr[x][y] = '*';
+			return;
+		}
+ 
+		/*
+		   N=27 일 경우 한 블록의 사이즈는 9이고, 
+		   N=9 일 경우 한 블록의 사이즈는 3이듯
+		   해당 블록의 한 칸을 담을 변수를 의미 size
+           
+		   count는 별 출력 누적을 의미
+		 */
+ 
+		int size = N / 3;
+		int count = 0;
+		for (int i = x; i < x + N; i += size) {
+			for (int j = y; j < y + N; j += size) {
+				count++;
+				if (count == 5) { // 공백 칸일 경우
+					star(i, j, size, true);
+				} else {
+					star(i, j, size, false);
+				}
+			}
+		}
+	}
 }
