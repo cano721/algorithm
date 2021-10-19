ebe577c49a5ccd16f22ad718855222a26210abf4
cano721 <cano721@naver.com>
2021-10-19 11:52:59 +0900

    Java 백준 분해합

---
+++ Java/BJ2231_분해합.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class BJ2231_분해합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i <= n; i++){
+            int num = i;
+            int checkNum = num;
+            while( num >0){
+                checkNum += num%10;
+                num /= 10;
+            }
+            if(checkNum == n){
+                System.out.println(i);
+                break;
+            }
+        }
+
+    }
+}
