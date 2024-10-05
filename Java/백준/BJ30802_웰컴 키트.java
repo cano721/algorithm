17f1e7d82634276b51f52c52fc9091d88484b1c8
계해범 <cano721@naver.com>
2024-10-05 16:03:22 +0900

    [알고리즘] 백준 - 웰컴 키트 java 풀이

--- Java/백준/BJ30802_웰컴 키트.java
+++ Java/백준/BJ30802_웰컴 키트.java
@@ -20,7 +20,6 @@ import java.util.Arrays;
  * 시간복잡도 : N
  */
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
